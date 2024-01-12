9868d6f3fdf50b9a051f1fc5933da5c90679dca6
amitabhiam <amitabhs132@gmail.com>
2024-01-13 04:04:00 +0530

    Update Sample.js

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 function Sample() {
-  return (
+    const [data, setData] = useState(0);
+
+    return (
     <div>
         <h1>This is the sample components files</h1>
     </div>
-  )
+    )
 }
 
 export default Sample;
